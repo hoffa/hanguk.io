77fffd821da8c918301b68d3a15e2cc6337de542
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2025-07-05 18:23:24 +0900

    basic compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import ViteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,14 @@ export default defineConfig({
   build: {
     outDir: 'docs',
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImagemin({
+      mozjpeg: {
+        quality: 40,
+        progressive: true,
+      },
+    }),
+  ],
 })
